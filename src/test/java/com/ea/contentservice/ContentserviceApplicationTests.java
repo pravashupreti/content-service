a7a8806a5de63f585900e2486ec6d6f55973eb11
pravash <pravash@pravash-mac.local>
2022-06-12 11:00:13 -0500

    Initialize project

---
+++ src/test/java/com/ea/contentservice/ContentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea.contentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
