6f1cde4e375a98b8af25eaf069c49c267e201793
pravash <pravash@pravash-mac.local>
2022-06-12 14:32:51 -0500

    Add payload classes

---
+++ src/main/java/com/ea/contentservice/payload/BlogResponse.java
@@ -0,0 +1,33 @@
+package com.ea.contentservice.payload;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+public class BlogResponse {
+	private Long id;
+
+	private String title;
+	private String body;
+	private String category;
+	private List<String> tags;
+
+
+
+	public List<String> getTags() {
+
+		return tags == null ? null : new ArrayList<>(tags);
+	}
+
+	public void setTags(List<String> tags) {
+
+		if (tags == null) {
+			this.tags = null;
+		} else {
+			this.tags = Collections.unmodifiableList(tags);
+		}
+	}
+}
