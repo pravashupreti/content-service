9b5a953499971a27ee3abf1d0a7f95830b9b1e96
pravash <pravash@pravash-mac.local>
2022-06-12 14:33:59 -0500

    Add utils classes

---
+++ src/main/java/com/ea/contentservice/utils/AppConstants.java
@@ -0,0 +1,21 @@
+package com.ea.contentservice.utils;
+
+public class AppConstants {
+	public static final String DEFAULT_PAGE_NUMBER = "0";
+
+	public static final String DEFAULT_PAGE_SIZE = "30";
+
+	public static final int MAX_PAGE_SIZE = 30;
+
+	public static final String CREATED_AT = "createdAt";
+
+	public static final String ID = "id";
+
+
+	public static final String CATEGORY = "Category";
+
+	public static final String TAG = "Tag";
+
+	public static final String BLOG = "Blog";
+
+}
