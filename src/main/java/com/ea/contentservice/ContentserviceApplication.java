16d5a5f042ca106b985f0943fc7e191b29da37f4
pravash <pravash@pravash-mac.local>
2022-06-12 14:30:45 -0500

    Add model mapper

--- src/main/java/com/ea/contentservice/ContentserviceApplication.java
+++ src/main/java/com/ea/contentservice/ContentserviceApplication.java
@@ -1,13 +1,32 @@
 package com.ea.contentservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.convert.Jsr310Converters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {ContentserviceApplication.class, Jsr310Converters.class})
+
 public class ContentserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ContentserviceApplication.class, args);
+    }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
